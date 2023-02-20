f366fde23e0bd3f0a2a5fa16e28efdbc6f0a608b
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-02-20 15:56:25 +0100

    :wrench: setup for sass

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio',
+  define: {
+    'process.env': {},
+  },
+  build: {
+    target: 'esnext',
+  },
+   css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@forward "./src/scss/index";'
+      },
+    },
+  },
   plugins: [vue()],
 })
