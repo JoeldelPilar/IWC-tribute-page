68c85f86f081e83eb2df9058904a1a1b8406e622
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-02-21 14:46:26 +0100

    :bento: add project imgs

---
+++ src/components/SiteHero.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="hero__container">
+		<div class="hero__background"></div>
+	</section>
+</template>
+
+<script lang="ts">
+	export default {
+		setup() {
+			return {};
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.hero__background {
+		background-image: url(../../public/pilot_iwc_collection.webp);
+	}
+</style>
