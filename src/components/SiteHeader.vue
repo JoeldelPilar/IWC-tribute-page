b0b055ff8c760705f42b681e8d023bd4634baf91
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-02-21 14:26:51 +0100

    add header styling

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,8 @@
 <template>
 	<header>
 		<div class="header__container">
-			<h1>The IWC Pilot - Mark XX</h1>
+			<h1 class="header__container--headline">The <span>IWC</span> Pilot</h1>
+			<span class="header__container--subheader">Mark XX</span>
 		</div>
 	</header>
 </template>
@@ -10,4 +11,28 @@
 	export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.header__container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		max-width: 80vw;
+		margin-inline: auto;
+	}
+	.header__container--headline {
+		margin-block-start: 2rem;
+		font-size: var(--step-2);
+		color: var(--clr-primary-800);
+
+		span {
+			font-family: var(--ff-secondary);
+		}
+	}
+
+	.header__container--subheader {
+		letter-spacing: 0.25rem;
+		font-size: var(--step-0);
+	}
+</style>
