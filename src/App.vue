494d44d8e5ec3420b0cc2583304bfc8155ecef07
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-02-23 08:55:16 +0100

    :lipstick: responsive hero img

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 	import SiteHeader from './components/SiteHeader.vue';
+	import SiteHero from './components/SiteHero.vue';
 </script>
 
 <template>
 	<SiteHeader />
+	<SiteHero />
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/SiteHero.vue
+++ src/components/SiteHero.vue
@@ -1,6 +1,8 @@
 <template>
 	<section class="hero__container">
-		<div class="hero__background"></div>
+		<div class="hero__background">
+			<p>The Classic that just got better!</p>
+		</div>
 	</section>
 </template>
 
@@ -14,6 +16,27 @@
 
 <style lang="scss" scoped>
 	.hero__background {
+		position: relative;
 		background-image: url(../../public/pilot_iwc_collection.webp);
+		background-repeat: no-repeat;
+		background-size: contain;
+		background-position: center;
+		width: 100%;
+		height: 65vw;
+		opacity: 0.7;
+		p {
+			isolation: isolate;
+			font-size: var(--step-0);
+			font-weight: bold;
+			opacity: 100%;
+		}
+	}
+
+	@media (width > 1440px) {
+		.hero__background {
+			background-size: auto;
+			padding: 20rem;
+			margin-block: 5rem;
+		}
 	}
 </style>
